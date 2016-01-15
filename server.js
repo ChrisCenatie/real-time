fa323847e966818a3486babccc63e290d3a1afd2
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-14 20:36:38 -0700

    Initial views and handlebars set up.

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const _ = require('lodash');
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+pry = require('pryjs');
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+ res.sendFile(__dirname + '/public/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+              .listen(port, function(){
+               console.log('Listening on port ' + port + '.');
+              });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
