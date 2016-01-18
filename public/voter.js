3f0bd44f86b9a9c93112ddd0f4830f1686780f36
Chris Cenatiempo <etothe2pieye@gmail.com>
2016-01-18 16:42:00 -0700

    Updated key names for object being passed to server.

--- public/voter.js
+++ public/voter.js
@@ -1,14 +1,15 @@
 var socket = io();
 var parsedUrl = window.location.href.split('/');
-var vote = {id: parsedUrl[parsedUrl.length - 1]};
+var vote = {pollId: parsedUrl[parsedUrl.length - 1]};
 console.log(vote);
 
 $('button').on('click', function() {
   var responseTitle = this.innerHTML;
-  vote["response"] = this.className;
+  vote["voteId"] = this.className;
+  console.log(vote);
   $('.responses').hide();
   $('.submitted').show();
   $('h3').append("Your vote <em>'" + responseTitle +
                          "'</em> has been submitted!");
-  socket.send("voteResponse", vote)
+  // socket.send("voteResponse", vote)
 })
